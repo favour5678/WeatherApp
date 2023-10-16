13ae9f45e5f70fb25827104d56051ad179ad10f9
Oshoaro Favour <oshoarofavour@gmail.com>
2023-10-16 12:52:56 +0100

    working on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Home } from "./pages/Home"
+
 function App() {
   return (
-    <>
-      Weather App
-    </>
+    <main>
+      <Home />
+    </main>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+    // #222850
+    // #8C43A8
+  return (
+    <section className='bg-gradient-to-l from-[#222850] to-[#8C43A8] w-full h-screen'>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam expedita aspernatur maxime eveniet ipsa quaerat soluta neque quam amet nemo, est repellendus assumenda voluptatum corrupti suscipit ipsum asperiores sapiente perspiciatis?
+        </div>
+    </section>
+  )
+}
