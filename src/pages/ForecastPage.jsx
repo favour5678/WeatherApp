83f6bc5f355c1602c96350a35c1aa200b3698a67
Oshoaro Favour <oshoarofavour@gmail.com>
2023-10-16 18:18:48 +0100

    installed a package & working on the forecast page

--- src/pages/ForecastPage.jsx
+++ src/pages/ForecastPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+const API_KEY = '3eZ0N3WeW347eIV3PzWc9hhN8dy4y00k'
+const BASE_URL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+
 export const ForecastPage = () => {
   return (
     <section>
-        <div>Wlll</div>
+        <div></div>
     </section>
   )
 }
